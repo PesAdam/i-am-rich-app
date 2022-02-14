f098c6c5e0e002b4ccc5057e87ab06ae11208642
PesAdam <pesadam15@gmail.com>
2022-02-14 21:44:21 +0100

    zaciname

---
+++ iamrichapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iamrichapp
+//
+//  Created by Bender on 14/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
